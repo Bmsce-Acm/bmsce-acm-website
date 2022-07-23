729413ff3b3237973d7219a870aa090c9683966b
pgp22 <pgprabhat2021@gmail.com>
2022-07-23 21:50:55 +0530

    1st

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       BMSCE ACM S-CHAP
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-
-	return (
-		<div>Header</div>
-	)
+  return (
+    //   HEADER
+    <header className="header">
+      <div className="header_hello" data-header>
+        <div className="container">
+          Logo
+          <nav className="navbar container" data-navbar>
+            <ul className="navbar-list">
+              <li>
+                <Link to="/home" className="navbar-link">
+                  Events
+                </Link>
+              </li>
+              <li>
+                <Link to="/home" className="navbar-link">
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link to="/home" className="navbar-link">
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link to="/home" className="navbar-link">
+                  Contact Us
+                </Link>
+              </li>
+              <li>
+                <Link to="/home" className="navbar-link">
+                  XYZ
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <Link to="/home" className="navbar-link">
+            Sign Up
+          </Link>
+          <button
+            className="nav-toggle-btn"
+            aria-label="Toggle menu"
+            data-nav-toggler
+          >
+            <ion-icon
+              name="menu-sharp"
+              aria-hidden="true"
+              className="menu-icon"
+            ></ion-icon>
+            <ion-icon
+              name="close-sharp"
+              aria-hidden="true"
+              className="close-icon"
+            ></ion-icon>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
